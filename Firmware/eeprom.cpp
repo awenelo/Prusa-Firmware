c373d66859f254815297051586ea4b92505dd625
awenelo <56133692+awenelo@users.noreply.github.com>
2021-03-08 09:09:47 -0500

    Correct logic

--- Firmware/eeprom.cpp
+++ Firmware/eeprom.cpp
@@ -73,7 +73,7 @@ void eeprom_init()
     
     for (uint_least8_t i = 0; i < (sizeof(Sheets::s)/sizeof(Sheets::s[0])); ++i)
     {
-        bool is_uninitialized = eeprom_is_sheet_initialized(i);
+        bool is_uninitialized = !eeprom_is_sheet_initialized(i);
         /*for (uint_least8_t j = 0; j < (sizeof(Sheet::name)/sizeof(Sheet::name[0])); ++j)
         {
             if (!eeprom_is_uninitialized(&(EEPROM_Sheets_base->s[i].name[j]))) is_uninitialized = false;
