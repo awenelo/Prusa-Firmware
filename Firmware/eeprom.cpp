da8ff93ce38339e32811264dcacbf3e3c8992224
awenelo <56133692+awenelo@users.noreply.github.com>
2021-03-08 10:11:28 -0500

    Clean up

--- Firmware/eeprom.cpp
+++ Firmware/eeprom.cpp
@@ -73,12 +73,7 @@ void eeprom_init()
     
     for (uint_least8_t i = 0; i < (sizeof(Sheets::s)/sizeof(Sheets::s[0])); ++i)
     {
-        bool is_uninitialized = !eeprom_is_sheet_initialized(i);
-        /*for (uint_least8_t j = 0; j < (sizeof(Sheet::name)/sizeof(Sheet::name[0])); ++j)
-        {
-            if (!eeprom_is_uninitialized(&(EEPROM_Sheets_base->s[i].name[j]))) is_uninitialized = false;
-        }*/
-        if(is_uninitialized)
+        if(!eeprom_is_sheet_initialized(i))
         {
             SheetName sheetName;
             eeprom_default_sheet_name(i,sheetName);
